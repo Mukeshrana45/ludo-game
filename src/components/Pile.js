97e3ad7a95ad16849be252e1a4df58379ea464cb
Mukesh Rana <mukeshotwalarana@gmail.com>
2026-06-12 19:07:02 +0530

    Add pocket layout and piece rendering with player state integration

--- src/components/Pile.js
+++ src/components/Pile.js
@@ -1,12 +1,113 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Animated, TouchableOpacity, Easing, Image } from 'react-native'
+import React, { memo, useCallback, useEffect, useMemo, useRef } from 'react'
+import { useSelector } from 'react-redux';
+import { selectCellSelection, selectDiceNo, selectPocketPilesSelection } from '../redux/reducers/gameSelectors';
+import { Colors } from '../constants/Colors';
+import PileGreen from '../assets/images/piles/green.png';
+import PileRed from '../assets/images/piles/red.png';
+import PileBlue from '../assets/images/piles/blue.png';
+import PileYellow from '../assets/images/piles/yellow.png';
 
-const Pile = () => {
-  return (
-    <View>
-      <Text>Pile</Text>
-    </View>
+const Pile = ({cell, pieceid, color, player, onPress}) => {
+  const rotation= useRef(new Animated.Value(0)).current;
+  const currentPlayerPileSelection= useSelector(selectPocketPilesSelection);
+  const currentPlayerCellSelection= useSelector(selectCellSelection);
+  const diceNo= useSelector(selectDiceNo);
+  const PlayerPieces = useSelector(state =>state.game[`player${player}`]);
+  const isPileEnabled= useMemo(
+    () => player === currentPlayerPileSelection,
+    [player, currentPlayerPileSelection],
+  );
+  const isCellEnabled= useMemo(
+    () => player === currentPlayerCellSelection,
+    [player, currentPlayerCellSelection],
+  );
+  const isForwardable = useCallback(() => {
+    const piece= PlayerPieces?.find(item => item.id === pieceid);
+    return piece && piece.travelCount + diceNo <= 57;
+
+  },[PlayerPieces, pieceid, diceNo]);
+  const getPileImage = useMemo(() => {
+    switch(color) {
+      case Colors.green:
+        return PileGreen;
+      case Colors.red:
+        return PileRed;
+      case Colors.blue:
+        return PileBlue;
+      case Colors.yellow:
+        return PileYellow;
+      default:
+        return PileGreen;
+    }
+  },[color]);
+  useEffect(() => {
+    const rotateAnimation = Animated.loop(
+      Animated.timing(rotation, {
+        toValue: 1,
+        duration: 1000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      }),
+    )
+    rotateAnimation.start();
+
+    return() => rotateAnimation.stop();
+  },[rotation])
+
+  const rotateInterpolate = useMemo(
+    () => 
+      rotation.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg','360deg'],
+      }),
+      [rotation],
   )
-}
 
-export default Pile
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      activeOpacity={0.5}
+       disabled={!(cell ? isCellEnabled && isForwardable(): isPileEnabled)}
+       onPress={onPress}>
+        <View style={styles.hollowCircle}></View>
+        <Image
+           source={getPileImage}
+           style={{width:32, height: 32, position: 'absolute'}}/>
+       </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    alignSelf: 'center',
+  },
+  hollowCircle: {
+    width: 15,
+    height: 15,
+    position: 'absolute',
+    borderRadius: 25,
+    borderWidth: 2,
+    borderColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  dashedCircleContainer: {
+    position: 'absolute',
+    width: 25,
+    height: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    top: -8,
+  },
+  dashedCircle: {
+    width: 25,
+    height: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
+export default memo(Pile);
